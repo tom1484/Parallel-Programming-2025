e680388824a3f825c27b99792a7921b0c0518b1f
tom1484 <tomchen2003611@gmail.com>
2025-09-28 13:09:02 +0800

    adjust debug output

--- hw1/include/bfs_solver.hpp
+++ hw1/include/bfs_solver.hpp
@@ -1,9 +1,14 @@
 #ifndef BFS_SOLVER_HPP
 #define BFS_SOLVER_HPP
 
+#include <unordered_set>
+
 #include "solver.hpp"
 
 class BFSSolver : public Solver {
+   private:
+    bool normalize_and_check_invalid(State& state, unordered_set<uint64_t>& visited) const;
+
    public:
     BFSSolver() {}
     vector<Direction> solve(const State& initial_state) override;
--- hw1/src/bfs_solver.cpp
+++ hw1/src/bfs_solver.cpp
@@ -2,45 +2,40 @@
 
 #include <iostream>
 #include <queue>
-#include <unordered_set>
 
 using namespace std;
 
 extern Game game;
 
-// TODO: Check which is faster:
-//       1. check visited after popping from queue
-//       2. check visited before pushing into queue (requires normalizing the state first)
+// Normalize the state and check if it's dead or visited
+bool BFSSolver::normalize_and_check_invalid(State& state, unordered_set<uint64_t>& visited) const {
+    state.normalize();
+    if (state.dead) return true;  // Dead state
+
+    uint64_t state_hash = state.hash();
+    if (visited.count(state_hash)) return true;  // Visited state
+    visited.insert(state_hash);
+    return false;
+}
+
 vector<Direction> BFSSolver::solve(const State& initial_state) {
     // State, (pushed box index, direction)
     queue<pair<State, vector<Move>>> q;
     unordered_set<uint64_t> visited;
 
-    int explored = 0;
     bool solved = false;
     vector<Move> solution;
 
-    q.push({initial_state, {}});
+    State curr_state = initial_state;
+    curr_state.normalize();
+
+    q.push({curr_state, {}});
     while (!q.empty()) {
         auto [curr_state, history] = q.front();
         q.pop();
 
-        // Normalize the state to find the connected component and available boxes
-        curr_state.normalize();
-        if (curr_state.dead) continue;  // Skip dead states
-
-        // Hash the state and check if visited
-        uint64_t state_hash = curr_state.hash();
-        if (visited.count(state_hash)) continue;
-        visited.insert(state_hash);
-
-// Debug info
-#ifdef DEBUG
-        explored++;
-        if (explored % 10000 == 0) {
-            cout << "Explored: " << explored << ", Queue size: " << q.size() << endl;
-        }
-#endif
+        // NOTE: This is tested to be slower for now
+        // if (normalize_and_check_invalid(curr_state, visited)) continue;
 
         Move prev_move = history.empty() ? Move{Position(), Direction::LEFT} : history.back();
         for (size_t box_id = 0; box_id < curr_state.available_boxes.size(); ++box_id) {
@@ -60,11 +55,18 @@ vector<Direction> BFSSolver::solve(const State& initial_state) {
                     break;
                 }
 
+                if (normalize_and_check_invalid(new_state, visited)) continue;
                 q.push({new_state, new_history});
             }
             if (solved) break;
         }
         if (solved) break;
+
+#ifdef DEBUG
+        if (visited.size() % 10000 == 0) {
+            cerr << "Explored: " << visited.size() << ", Queue size: " << q.size() << endl;
+        }
+#endif
     }
 
     // cout << "Solution length: " << solution.size() << endl;
