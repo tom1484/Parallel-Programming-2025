13dc0275a65d10b79e2d93fc3769a4520576cb1e
tom1484 <tomchen2003611@gmail.com>
2025-10-02 01:34:47 +0800

    add A* solver with empty heuristic

--- hw1/include/a_star_solver.hpp
+++ hw1/include/a_star_solver.hpp
@@ -1,14 +1,53 @@
 #ifndef A_STAR_SOLVER_HPP
 #define A_STAR_SOLVER_HPP
 
+#include <optional>
+#include <queue>
+#include <unordered_map>
 #include <vector>
 
 #include "solver.hpp"
 
+typedef vector<pair<Move, size_t>> History;       // ((current move), previous history index)
+typedef unordered_map<uint64_t, size_t> Visited;  // (state hash, history index)
+typedef struct Node {
+    uint32_t depth;
+    uint32_t heuristic;
+    State state;
+    size_t history_idx;
+} Node;
+typedef priority_queue<Node, vector<Node>, greater<Node>> PQueue;
+
+bool operator>(const Node& a, const Node& b) {
+    uint64_t a_combined = ((uint64_t)(a.depth) << 32) + a.heuristic;
+    uint64_t b_combined = ((uint64_t)(b.depth) << 32) + b.heuristic;
+    return a_combined > b_combined;
+}
+
 class AStarSolver : public Solver {
+   private:
+    PQueue forward_queue;
+    PQueue backward_queue;
+    // Hashmap of visited states and the histories index
+    Visited forward_visited;
+    Visited backward_visited;
+    // Store the history of each visited state
+    History forward_history;
+    History backward_history;
+
+    optional<pair<uint64_t, size_t>> normalize_and_insert_history(State& state, StateMode mode,
+                                                                  const pair<Move, size_t>& new_op);
+    void forward_step();
+    void backward_step();
+    void construct_solution(size_t forward_history_idx, size_t backward_history_idx);
+
    public:
     AStarSolver(const State& initial_state) : Solver(initial_state) {}
     vector<Direction> solve() override;
+#ifdef DEBUG
+    vector<Direction> forward_solve();
+    vector<Direction> backward_solve();
+#endif
 };
 
 #endif  // A_STAR_SOLVER_HPP
--- hw1/src/a_star_solver.cpp
+++ hw1/src/a_star_solver.cpp
@@ -1,7 +1,232 @@
 #include "a_star_solver.hpp"
 
-using namespace std;
+#include <algorithm>
+#include <iostream>
+#include <optional>
+#include <queue>
+#include <unordered_map>
 
 extern Game game;
 
-vector<Direction> AStarSolver::solve() { return {}; }
+// Normalize the state and check if it's dead or visited
+optional<pair<uint64_t, size_t>> AStarSolver::normalize_and_insert_history(State& state, StateMode mode,
+                                                                           const pair<Move, size_t>& new_op) {
+    state.normalize(mode);
+    if (state.dead) return nullopt;  // Dead state
+
+    Visited& visited = (mode == StateMode::PUSH) ? forward_visited : backward_visited;
+    History& history = (mode == StateMode::PUSH) ? forward_history : backward_history;
+
+    uint64_t state_hash = state.hash();
+    if (visited.count(state_hash)) return nullopt;  // Visited state
+    size_t history_idx = history.size();
+    visited[state_hash] = history_idx;
+    history.push_back(new_op);
+
+    // Hashing is expensive, return the hash value to avoid recomputation
+    return {{state_hash, history_idx}};
+}
+
+void AStarSolver::forward_step() {
+    PQueue& q = forward_queue;
+    auto [curr_depth, _, curr_state, history_idx] = q.top();
+    q.pop();
+
+    const Move& prev_move = (history_idx != (size_t)(-1)) ? forward_history[history_idx].first : Move{};
+    for (size_t box_id = 0; box_id < curr_state.reachable_boxes.size(); ++box_id) {
+        Position box = curr_state.reachable_boxes[box_id];
+        vector<Direction> pushes = curr_state.available_pushes(box_id);
+
+        for (const Direction& dir : pushes) {
+            if (box == prev_move.first && dir == dir_inv(prev_move.second)) continue;  // Avoid pushing back
+
+            State new_state = curr_state.push(box_id, dir);
+            auto insert_result = normalize_and_insert_history(new_state, StateMode::PUSH, {{box, dir}, history_idx});
+            if (!insert_result) continue;
+            auto& [state_hash, new_history_idx] = insert_result.value();
+
+            // Check if we meet the backward search
+            if (backward_visited.count(state_hash)) {
+                construct_solution(new_history_idx, backward_visited[state_hash]);
+                return;
+            }
+            // Check if we reach the target
+            if (new_state.boxes == game.targets) {
+                construct_solution(new_history_idx, -1);
+                return;
+            }
+
+            q.push({curr_depth + 1, 0, new_state, new_history_idx});
+        }
+    }
+}
+
+void AStarSolver::backward_step() {
+    PQueue& q = backward_queue;
+    auto [curr_depth, _, curr_state, history_idx] = q.top();
+    q.pop();
+
+    const Move& prev_move = (history_idx != (size_t)(-1)) ? backward_history[history_idx].first : Move{};
+    for (size_t box_id = 0; box_id < curr_state.reachable_boxes.size(); ++box_id) {
+        Position box = curr_state.reachable_boxes[box_id];
+        vector<Direction> pulls = curr_state.available_pulls(box_id);
+
+        for (const Direction& dir : pulls) {
+            if (box == prev_move.first && dir == dir_inv(prev_move.second)) continue;  // Avoid pushing back
+
+            State new_state = curr_state.pull(box_id, dir);
+            auto insert_result = normalize_and_insert_history(new_state, StateMode::PULL, {{box, dir}, history_idx});
+            if (!insert_result) continue;
+            auto& [state_hash, new_history_idx] = insert_result.value();
+
+            // Check if we meet the forward search
+            if (forward_visited.count(state_hash)) {
+                construct_solution(forward_visited[state_hash], new_history_idx);
+                return;
+            }
+            // Check if we meet the initial state
+            if (new_state.boxes == game.initial_boxes && new_state.reachable[initial_state.player.to_index()]) {
+                construct_solution(-1, new_history_idx);
+                return;
+            }
+
+            q.push({curr_depth + 1, 0, new_state, new_history_idx});
+        }
+    }
+}
+
+void AStarSolver::construct_solution(size_t forward_history_idx, size_t backward_history_idx) {
+    vector<Move> history;
+    for (size_t i = forward_history_idx; i != (size_t)(-1); i = forward_history[i].second) {
+        const auto& [move, prev_idx] = forward_history[i];
+        history.push_back(move);
+    }
+    reverse(history.begin(), history.end());
+    for (size_t i = backward_history_idx; i != (size_t)(-1); i = backward_history[i].second) {
+        const auto& [move, prev_idx] = backward_history[i];
+        const auto& [box, dir] = move;
+        history.push_back({box + dir, dir_inv(dir)});
+    }
+    solution = history;
+    solved = true;
+}
+
+#ifdef DEBUG
+vector<Direction> AStarSolver::forward_solve() {
+    if (game.targets == initial_state.boxes) return {};  // Already solved
+    int last_print = 0;
+
+    // State, (pushed box index, direction)
+    State curr_state = initial_state;
+    normalize_and_insert_history(curr_state, StateMode::PUSH, {Move(), -1});
+    forward_queue.push({0, 0, curr_state, (size_t)(-1)});
+
+    while (!solved && !forward_queue.empty()) {
+        forward_step();
+#ifdef DEBUG
+        int total_explored = forward_visited.size();
+        if (total_explored >= last_print + 10000) {
+            cerr << "Explored: " << total_explored << endl;
+            last_print += 10000;
+        }
+#endif
+    }
+
+    return expand_solution(initial_state);
+}
+#endif
+
+#ifdef DEBUG
+vector<Direction> AStarSolver::backward_solve() {
+    if (game.targets == initial_state.boxes) return {};  // Already solved
+    int last_print = 0;
+
+    // State, (pushed box index, direction)
+    State curr_state = initial_state;
+    curr_state.boxes = game.targets;  // Inverse the target and initial state
+    normalize_and_insert_history(curr_state, StateMode::PULL, {Move(), -1});
+    backward_queue.push({0, 0, curr_state, (size_t)(-1)});
+
+    // Permute different end states
+    {
+        Map block = game.box_map | curr_state.boxes;
+        for (uint8_t y = 0; y < game.height; ++y) {
+            for (uint8_t x = 0; x < game.width; ++x) {
+                Position pos(x, y);
+                uint16_t idx = pos.to_index();
+                if (block[idx] || curr_state.reachable[idx]) continue;
+
+                State possible_state = curr_state;
+                possible_state.reset();
+                possible_state.player = pos;
+                normalize_and_insert_history(possible_state, StateMode::PULL, {Move(), -1});
+                backward_queue.push({0, 0, possible_state, (size_t)(-1)});
+
+                curr_state.reachable |= possible_state.reachable;
+            }
+        }
+    }
+
+    while (!solved && !backward_queue.empty()) {
+        backward_step();
+        int total_explored = backward_visited.size();
+        if (total_explored >= last_print + 10000) {
+            cerr << "Explored: " << total_explored << endl;
+            last_print += 10000;
+        }
+    }
+
+    return expand_solution(initial_state);
+}
+#endif
+
+vector<Direction> AStarSolver::solve() {
+    if (game.targets == initial_state.boxes) return {};  // Already solved
+#ifdef DEBUG
+    int last_print = 0;
+#endif
+
+    State curr_state = initial_state;
+    // Initialize forward search
+    normalize_and_insert_history(curr_state, StateMode::PUSH, {Move(), -1});
+    forward_queue.push({0, 0, curr_state, (size_t)(-1)});
+    // Initialize backward search
+    curr_state.boxes = game.targets;  // Inverse the target and initial state
+    normalize_and_insert_history(curr_state, StateMode::PULL, {Move(), -1});
+    backward_queue.push({0, 0, curr_state, (size_t)(-1)});
+    // Permute different end states for backward search
+    {
+        Map block = game.box_map | curr_state.boxes;
+        for (uint8_t y = 0; y < game.height; ++y) {
+            for (uint8_t x = 0; x < game.width; ++x) {
+                Position pos(x, y);
+                uint16_t idx = pos.to_index();
+                if (block[idx] || curr_state.reachable[idx]) continue;
+
+                State possible_state = curr_state;
+                possible_state.reset();
+                possible_state.player = pos;
+                normalize_and_insert_history(possible_state, StateMode::PULL, {Move(), -1});
+                backward_queue.push({0, 0, possible_state, (size_t)(-1)});
+
+                curr_state.reachable |= possible_state.reachable;
+            }
+        }
+    }
+
+    while (!solved && !forward_queue.empty() && !backward_queue.empty()) {
+        if (forward_queue.size() <= backward_queue.size())
+            forward_step();
+        else
+            backward_step();
+#ifdef DEBUG
+        int total_explored = forward_visited.size() + backward_visited.size();
+        if (total_explored >= last_print + 10000) {
+            cerr << "Explored: " << total_explored << endl;
+            last_print += 10000;
+        }
+#endif
+    }
+
+    return expand_solution(initial_state);
+}
