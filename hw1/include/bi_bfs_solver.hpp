decf4b92e1a9940ae7efa5266ab18e674e666138
tom1484 <tomchen2003611@gmail.com>
2025-10-01 21:18:30 +0800

    memorize all states for bi-directional BFS to expand solution

--- hw1/include/bi_bfs_solver.hpp
+++ hw1/include/bi_bfs_solver.hpp
@@ -2,20 +2,38 @@
 #define BI_BFS_SOLVER_HPP
 
 #include <queue>
-#include <unordered_set>
+#include <unordered_map>
 #include <vector>
+#include <optional>
 
 #include "solver.hpp"
 
+typedef vector<pair<Move, size_t>> History;       // ((current move), previous history index)
+typedef pair<State, size_t> Node;                 // (state, history)
+typedef unordered_map<uint64_t, size_t> Visited;  // (state hash, history index)
+
 class BiBFSSolver : public Solver {
    private:
-    bool normalize_and_check(State& state, StateMode mode, unordered_set<uint64_t>& visited) const;
-    void forward_step(queue<pair<State, vector<Move>>>& q, unordered_set<uint64_t>& visited);
-    void backward_step(queue<pair<State, vector<Move>>>& q, unordered_set<uint64_t>& visited);
+    queue<Node> forward_queue;
+    queue<Node> backward_queue;
+    // Hashmap of visited states and the histories index
+    Visited forward_visited;
+    Visited backward_visited;
+    // Store the history of each visited state
+    History forward_history;
+    History backward_history;
+
+    optional<size_t> normalize_and_insert_history(State& state, StateMode mode, const pair<Move, size_t>& new_op);
+    void forward_step();
+    void backward_step();
 
    public:
     BiBFSSolver(const State& initial_state) : Solver(initial_state) {}
     vector<Direction> solve() override;
+#ifdef DEBUG
+    vector<Direction> forward_solve();
+    vector<Direction> backward_solve();
+#endif
 };
 
 #endif  // BI_BFS_SOLVER_HPP
--- hw1/src/bi_bfs_solver.cpp
+++ hw1/src/bi_bfs_solver.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <iostream>
+#include <optional>
 #include <queue>
 #include <unordered_set>
 
@@ -10,21 +11,29 @@ using namespace std;
 extern Game game;
 
 // Normalize the state and check if it's dead or visited
-bool BiBFSSolver::normalize_and_check(State& state, StateMode mode, unordered_set<uint64_t>& visited) const {
+optional<size_t> BiBFSSolver::normalize_and_insert_history(State& state, StateMode mode,
+                                                           const pair<Move, size_t>& new_op) {
     state.normalize(mode);
-    if (state.dead) return true;  // Dead state
+    if (state.dead) return nullopt;  // Dead state
+
+    Visited& visited = (mode == StateMode::PUSH) ? forward_visited : backward_visited;
+    History& history = (mode == StateMode::PUSH) ? forward_history : backward_history;
 
     uint64_t state_hash = state.hash();
-    if (visited.count(state_hash)) return true;  // Visited state
-    visited.insert(state_hash);
-    return false;
+    if (visited.count(state_hash)) return nullopt;  // Visited state
+    size_t history_idx = history.size();
+    visited[state_hash] = history_idx;
+    history.push_back(new_op);
+
+    return {history_idx};
 }
 
-void BiBFSSolver::forward_step(queue<pair<State, vector<Move>>>& q, unordered_set<uint64_t>& visited) {
-    auto [curr_state, history] = q.front();
+void BiBFSSolver::forward_step() {
+    queue<Node>& q = forward_queue;
+    auto [curr_state, history_idx] = q.front();
     q.pop();
 
-    Move prev_move = history.empty() ? Move{} : history.back();
+    const Move& prev_move = (history_idx != (size_t)(-1)) ? forward_history[history_idx].first : Move{};
     for (size_t box_id = 0; box_id < curr_state.reachable_boxes.size(); ++box_id) {
         Position box = curr_state.reachable_boxes[box_id];
         vector<Direction> pushes = curr_state.available_pushes(box_id);
@@ -32,27 +41,35 @@ void BiBFSSolver::forward_step(queue<pair<State, vector<Move>>>& q, unordered_se
         for (const Direction& dir : pushes) {
             if (box == prev_move.first && dir == dir_inv(prev_move.second)) continue;  // Avoid pushing back
 
-            vector<Move> new_history = history;
-            new_history.push_back({box, dir});
-
             State new_state = curr_state.push(box_id, dir);
+            optional<size_t> insert =
+                normalize_and_insert_history(new_state, StateMode::PUSH, {{box, dir}, history_idx});
+            if (!insert) continue;
+            size_t new_history_idx = insert.value();
+
             if (new_state.boxes == game.targets) {
                 solved = true;
-                solution = new_history;
+                vector<Move> history;
+                for (size_t i = new_history_idx; i != (size_t)(-1); i = forward_history[i].second) {
+                    const auto& [move, prev_idx] = forward_history[i];
+                    history.push_back(move);
+                }
+                reverse(history.begin(), history.end());
+                solution = history;
                 return;
             }
 
-            if (normalize_and_check(new_state, StateMode::PUSH, visited)) continue;
-            q.push({new_state, new_history});
+            q.push({new_state, new_history_idx});
         }
     }
 }
 
-void BiBFSSolver::backward_step(queue<pair<State, vector<Move>>>& q, unordered_set<uint64_t>& visited) {
-    auto [curr_state, history] = q.front();
+void BiBFSSolver::backward_step() {
+    queue<Node>& q = backward_queue;
+    auto [curr_state, history_idx] = q.front();
     q.pop();
 
-    Move prev_move = history.empty() ? Move{} : history.back();
+    const Move& prev_move = (history_idx != (size_t)(-1)) ? backward_history[history_idx].first : Move{};
     for (size_t box_id = 0; box_id < curr_state.reachable_boxes.size(); ++box_id) {
         Position box = curr_state.reachable_boxes[box_id];
         vector<Direction> pulls = curr_state.available_pulls(box_id);
@@ -60,37 +77,66 @@ void BiBFSSolver::backward_step(queue<pair<State, vector<Move>>>& q, unordered_s
         for (const Direction& dir : pulls) {
             if (box == prev_move.first && dir == dir_inv(prev_move.second)) continue;  // Avoid pushing back
 
-            vector<Move> new_history = history;
-            new_history.push_back({box, dir});
-
             State new_state = curr_state.pull(box_id, dir);
-            if (normalize_and_check(new_state, StateMode::PULL, visited)) continue;
+            optional<size_t> insert =
+                normalize_and_insert_history(new_state, StateMode::PULL, {{box, dir}, history_idx});
+            if (!insert) continue;
+            size_t new_history_idx = insert.value();
 
             if (new_state.boxes == game.initial_boxes && new_state.reachable[initial_state.player.to_index()]) {
                 solved = true;
                 vector<Move> inv_history;
-                for (const auto& [box, dir] : new_history) inv_history.push_back({box + dir, dir_inv(dir)});
-                reverse(inv_history.begin(), inv_history.end());
+                for (size_t i = new_history_idx; i != (size_t)(-1); i = backward_history[i].second) {
+                    const auto& [move, prev_idx] = backward_history[i];
+                    const auto& [box, dir] = move;
+                    inv_history.push_back({box + dir, dir_inv(dir)});
+                }
                 solution = inv_history;
                 return;
             }
-            q.push({new_state, new_history});
+
+            q.push({new_state, new_history_idx});
         }
     }
 }
 
-// NOTE: Below is a simple version of inverse BFS without meeting in the middle
-vector<Direction> BiBFSSolver::solve() {
+vector<Direction> BiBFSSolver::forward_solve() {
     if (game.targets == initial_state.boxes) return {};  // Already solved
+    
+#ifdef DEBUG
+    int last_print = 0;
+#endif
 
     // State, (pushed box index, direction)
-    queue<pair<State, vector<Move>>> q;
-    unordered_set<uint64_t> visited;
+    State curr_state = initial_state;
+    normalize_and_insert_history(curr_state, StateMode::PUSH, {Move(), -1});
+    forward_queue.push({curr_state, -1});
 
+    while (!solved && !forward_queue.empty()) {
+        forward_step();
+#ifdef DEBUG
+        int total_explored = forward_visited.size();
+        if (total_explored >= last_print + 1000) {
+            cerr << "Explored: " << total_explored << endl;
+            last_print += 1000;
+        }
+#endif
+    }
+
+    return expand_solution(initial_state);
+}
+
+vector<Direction> BiBFSSolver::backward_solve() {
+    if (game.targets == initial_state.boxes) return {};  // Already solved
+#ifdef DEBUG
+    int last_print = 0;
+#endif
+
+    // State, (pushed box index, direction)
     State curr_state = initial_state;
     curr_state.boxes = game.targets;  // Inverse the target and initial state
-    normalize_and_check(curr_state, StateMode::PULL, visited);
-    q.push({curr_state, {}});
+    normalize_and_insert_history(curr_state, StateMode::PULL, {Move(), -1});
+    backward_queue.push({curr_state, -1});
 
     // Permute different end states
     {
@@ -104,17 +150,38 @@ vector<Direction> BiBFSSolver::solve() {
                 State possible_state = curr_state;
                 possible_state.reset();
                 possible_state.player = pos;
-                normalize_and_check(possible_state, StateMode::PULL, visited);
-                q.push({possible_state, {}});
+                normalize_and_insert_history(possible_state, StateMode::PULL, {Move(), -1});
+                backward_queue.push({possible_state, -1});
 
                 curr_state.reachable |= possible_state.reachable;
             }
         }
     }
 
-    while (!solved && !q.empty()) {
-        backward_step(q, visited);
+    while (!solved && !backward_queue.empty()) {
+        backward_step();
+#ifdef DEBUG
+        int total_explored = backward_visited.size();
+        if (total_explored >= last_print + 1000) {
+            cerr << "Explored: " << total_explored << endl;
+            last_print += 1000;
+        }
+#endif
     }
+
+    return expand_solution(initial_state);
+}
+
+
+vector<Direction> BiBFSSolver::solve() {
+    if (game.targets == initial_state.boxes) return {};  // Already solved
+#ifdef DEBUG
+    int last_print = 0;
+#endif
+
+    // State, (pushed box index, direction)
+    State curr_state = initial_state;
+    normalize_and_insert_history(curr_state, StateMode::PUSH, {Move(), -1});
 
     return expand_solution(initial_state);
 }
