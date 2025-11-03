b9fe07da2c2ad78247eadedc3d0477f4a3df0e67
tom1484 <tomchen2003611@gmail.com>
2025-11-03 21:23:22 +0800

    migrate benchmark

--- hw4/scripts/benchmark.py
+++ hw4/scripts/benchmark.py
@@ -5,7 +5,6 @@ import numpy as np
 
 from run_testcase import (
     run_build,
-    read_testcase,
     run_testcase,
     validate,
     make_output_path,
@@ -33,7 +32,7 @@ def parse_arguments():
         help="Number of iterations for each testcase.",
     )
     parser.add_argument(
-        "--cpu",
+        "--sample",
         action="store_true",
         required=False,
         help="Use CPU version of the program.",
@@ -45,9 +44,6 @@ def parse_arguments():
         help="Skip building the project.",
     )
     parser.add_argument(
-        "--debug", action="store_true", required=False, help="Enable debug mode."
-    )
-    parser.add_argument(
         "--input_dir",
         type=str,
         default="assets/testcases",
@@ -56,7 +52,7 @@ def parse_arguments():
     parser.add_argument(
         "--output_dir",
         type=str,
-        default="results",
+        default="outputs",
         help="Directory to store output results.",
     )
     return parser.parse_args()
@@ -67,13 +63,13 @@ if __name__ == "__main__":
     args.save_log = True
 
     if not args.no_build:
-        run_build(args.debug, args.cpu)
+        run_build(args.sample)
 
     testcase_list = []
     for file in os.listdir(args.input_dir):
-        if file[-4:] != ".txt":
+        if file[-3:] != ".in":
             continue
-        id = int(file[:-4])
+        id = int(file[4:-3])
         if args.start_id <= id <= args.end_id:
             testcase_list.append(id)
 
@@ -86,28 +82,27 @@ if __name__ == "__main__":
 
     print(f"Running {len(testcase_list)} testcases...")
 
-    print("============================================")
-    print("| ID | Time (us) | Error (%) | Valid Iters |")
-    print("--------------------------------------------")
+    print("================================")
+    print("| ID | Time (us) | Valid Iters |")
+    print("--------------------------------")
 
     for id in testcase_list:
-        testcase_file = os.path.join(args.input_dir, f"{id:02d}.txt")
-        testcase_data = read_testcase(testcase_file)
-
         results = []
         elapsed_times = []
-        error_percentages = []
+        is_valids = []
         all_passed = True
 
         with open(output_path, "a") as f:
             f.write(f"Testcase ID: {id:02d}\n")
 
         for i in range(args.iter):
-            result = run_testcase(id=id, data=testcase_data, args=args)
+            result = run_testcase(id=id, args=args)
             if result["elapsed_time"] is not None and result["success"]:
                 elapsed_times.append(result["elapsed_time"])
-                error_percentage = validate(result["output_path"], result["valid_path"])
-                error_percentages.append(error_percentage)
+                is_valid = validate(
+                    result["input_path"], result["output_path"], result["valid_path"]
+                )
+                is_valids.append(is_valid)
             else:
                 all_passed = False
 
@@ -118,10 +113,14 @@ if __name__ == "__main__":
                 f.write(f"    Elapsed Time: {result['elapsed_time']} us\n")
                 f.write(f"    Success: {result['success']}\n")
                 if result["elapsed_time"] is not None and result["success"]:
-                    error_percentage = validate(result["output_path"], result["valid_path"])
-                    f.write(f"    Error Percentage: {error_percentage:.4f} %\n")
+                    is_valid = validate(
+                        result["input_path"],
+                        result["output_path"],
+                        result["valid_path"],
+                    )
+                    f.write(f"    Is Valid: {'Yes' if is_valid else 'No'}\n")
 
-        all_passed = all_passed and all(ep < 3.0 for ep in error_percentages)
+        all_passed = all_passed and all(is_valids)
 
         print(f"| {id:02d} ", end="")
 
@@ -131,13 +130,7 @@ if __name__ == "__main__":
         else:
             print(f"|       N/A ", end="")
 
-        if len(error_percentages) > 0:
-            error_percentage = np.mean(error_percentages)
-            print(f"| {error_percentage:9.4f} ", end="")
-        else:
-            print(f"|       N/A ", end="")
-
-        valid_iters_str = f"{len(elapsed_times)}/{args.iter}"
+        valid_iters_str = f"{sum(list(map(int, is_valids)))}/{args.iter}"
         print(f"| {valid_iters_str:>11s} |")
 
         with open(output_path, "a") as f:
@@ -146,10 +139,6 @@ if __name__ == "__main__":
                 f.write(f"    Mean Elapsed Time: {mean_elapsed_time} us\n")
             else:
                 f.write(f"    Mean Elapsed Time: N/A\n")
-            if len(error_percentages) > 0:
-                f.write(f"    Mean Error Percentage: {error_percentage:.4f} %\n")
-            else:
-                f.write(f"    Mean Error Percentage: N/A\n")
             f.write(f"    All Passed: {'Yes' if all_passed else 'No'}\n")
             f.write("\n")
 
